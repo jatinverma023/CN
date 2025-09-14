49fb5e1836514edc9cbb961ed719ac4c9abd0caa
Jatin Verma <jatinverma90900@gmail.com>
2025-09-14 23:09:49 +0530

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -1,64 +1,47 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
-class LeakyBucket {
-    private int[] q;
-    private int front = 0, rear = 0, capacity;
-
-    public LeakyBucket(int size) {
-        capacity = size;
-        q = new int[size];
-    }
-
-    // Insert packet into bucket
-    public void insert(int ele) {
-        if (rear == capacity) {
-            System.out.println("Bucket full! Packet lost: " + ele);
-        } else {
-            q[rear++] = ele;
-            System.out.println("Packet inserted: " + ele);
-        }
-    }
-
-    // Leak packets one by one at fixed rate
-    public void leak() {
-        if (rear == front) {
-            System.out.println("Bucket empty. No packets to leak.");
-        } else {
-            System.out.println("\nLeaking packets...");
-            while (front < rear) {
-                try {
-                    Thread.sleep(1000); // 1 second delay per leak
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Leaked packet: " + q[front]);
-                front++;
-            }
-        }
-    }
-}
-
-public class Leaky {
-    public static void main(String[] args) {
+class RSA {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        // User defines bucket size
-        System.out.print("Enter the bucket (queue) capacity: ");
-        int size = sc.nextInt();
-        LeakyBucket bucket = new LeakyBucket(size);
+        // Input message
+        System.out.print("Enter the message (as integer): ");
+        BigInteger msg = BigInteger.valueOf(sc.nextInt());
 
-        // User inputs packets
-        System.out.print("Enter number of packets to send: ");
-        int n = sc.nextInt();
+        // Input primes
+        System.out.print("Enter the first prime number: ");
+        BigInteger p = BigInteger.valueOf(sc.nextInt());
 
-        System.out.println("Enter the packets:");
-        for (int i = 0; i < n; i++) {
-            int ele = sc.nextInt();
-            bucket.insert(ele);
+        System.out.print("Enter the second prime number: ");
+        BigInteger q = BigInteger.valueOf(sc.nextInt());
+
+        // Compute n and z
+        BigInteger n = p.multiply(q);
+        BigInteger z = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+        System.out.println("The value of z = " + z);
+
+        // Find e (public exponent)
+        BigInteger e = BigInteger.TWO;
+        while (e.compareTo(z) < 0) {
+            if (e.gcd(z).equals(BigInteger.ONE)) {
+                break;
+            }
+            e = e.add(BigInteger.ONE);
         }
+        System.out.println("The value of e = " + e);
 
-        // Leak packets
-        bucket.leak();
+        // Find d (private exponent) using modInverse
+        BigInteger d = e.modInverse(z);
+        System.out.println("The value of d = " + d);
+
+        // Encrypt: c = msg^e mod n
+        BigInteger c = msg.modPow(e, n);
+        System.out.println("Encrypted message is : " + c);
+
+        // Decrypt: msgback = c^d mod n
+        BigInteger msgback = c.modPow(d, n);
+        System.out.println("Decrypted message is : " + msgback);
 
         sc.close();
     }
